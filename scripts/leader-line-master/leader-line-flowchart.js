72e105afa862bdcb3395482b2f482275f40a88f6
Walser52 <walser52@gmail.com>
2025-08-05 23:21:55 +0500

    m

--- scripts/leader-line-master/leader-line-flowchart.js
+++ scripts/leader-line-master/leader-line-flowchart.js
@@ -1,31 +1,3 @@
-
-// <script>
-// window.addEventListener('load', () => {
-//   // Define your reusable line style
-//   const flowLineStyle = {
-//     color: '#e74c3c',
-//     size: 4,
-//     path: 'fluid',
-//     endPlug: 'arrow',
-//     // dash: { len: 6, gap: 4 },
-//     // startPlug: 'disc',
-//     dropShadow: true
-//   };
-
-//   // Loop through all elements with `data-connect`
-//   document.querySelectorAll('[data-connect]').forEach(el => {
-//     const targets = el.dataset.connect.split(',');
-//     targets.forEach(targetId => {
-//       const target = document.getElementById(targetId.trim());
-//       if (target) {
-//         new LeaderLine(el, target, flowLineStyle);
-//       }
-//     });
-//   });
-// });
-// </script>
-
-
 <script>
 window.addEventListener('load', () => {
   const flowLineStyle = {
